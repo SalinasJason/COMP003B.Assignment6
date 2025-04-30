39d71245c6c7f4d6ba51b26fadd924d7c8a8ed87
SalinasJason <jasonsalinas770@gmail.com>
2025-04-30 15:37:47 -0700

    Added a Book model with properties

---
+++ COMP003B.Assignment6/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment6.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        // Collection navigation property
+        public virtual ICollection<BookAuthor>? BookAuthors { get; set; }
+    }
+}
